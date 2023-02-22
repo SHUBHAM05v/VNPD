89a9a6f62e33bbb486701a2ec7d61cb21f89974c
SHUBHAM SINGH <95577953+SHUBHAM05v@users.noreply.github.com>
2023-02-22 15:54:22 +0530

    Update Number_plate_detection.py

--- Number_plate_detection.py
+++ Number_plate_detection.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
+import pytesseract
 
-frameWidth = 640    #Frame Width
-franeHeight = 480   # Frame Height
+frameWidth = 640
+franeHeight = 480
 
-plateCascade = cv2.CascadeClassifier("D:\SACHIN\haarcascade_russian_plate_number.xml")
+plateCascade = cv2.CascadeClassifier("C:\VNPR\haarcascade_russian_plate_number.xml")
 minArea = 500
 
 cap =cv2.VideoCapture(0)
@@ -23,13 +24,13 @@ while True:
     for (x, y, w, h) in numberPlates:
         area = w*h
         if area > minArea:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.putText(img,"NumberPlate",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(img,"NumberPlate",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
             imgRoi = img[y:y+h,x:x+w]
             cv2.imshow("ROI",imgRoi)
     cv2.imshow("Result",img)
-    if cv2.waitKey(1) & 0xFF ==ord('s'):
-        cv2.imwrite("D:\SACHIN\cascade\IMAGES"+str(count)+".jpg",imgRoi)
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
+        cv2.imwrite("C:\VNPR\Saved NP"+str(count)+".jpg",imgRoi)
         cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
         cv2.putText(img,"Scan Saved",(15,265),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255),2)
         cv2.imshow("Result",img)
